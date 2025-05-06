de33df8fc6dd9be138e36d071795a4797f9237bc
Umar <uj2849@gmail.com>
2025-05-05 18:02:51 -0700

    Navbar back to default

--- ecommerce-cloth-store/src/customer/components/Navbar/Navbar.js
+++ ecommerce-cloth-store/src/customer/components/Navbar/Navbar.js
@@ -267,7 +267,7 @@ export default function Navbar() {
                       onClick={handleOpen}
                       className=" font-medium text-grey-900 hover:text-gray-800"
                     >
-                      Signin(Now is new change)
+                      Signin
                     </Button>
                   </>
                 )}
