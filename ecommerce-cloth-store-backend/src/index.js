660d2a41293745026efbaae79bc81de79b98fe8f
Umar <uj2849@gmail.com>
2025-06-20 18:47:01 +0500

    vercel file added

--- ecommerce-cloth-store-backend/src/index.js
+++ ecommerce-cloth-store-backend/src/index.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
