05c586564c7038314d5f5389c6d61302cfa5e8da
Umar <uj2849@gmail.com>
2025-06-22 19:49:50 +0500

    changed maded

--- ecommerce-cloth-store-backend/src/server.js
+++ ecommerce-cloth-store-backend/src/server.js
@@ -4,6 +4,16 @@ const connectDB = require("./Utils/DB");
 
 const PORT = process.env.REACT_APP_SERVER_PORT || 5007;
 
+// Replace existing PRODUCTION block with:
+if (process.env.REACT_APP_NODE_ENV === "PRODUCTION") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Ecommerce store backend listening on PORT ${PORT}`);
